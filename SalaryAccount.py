0b65bfd6328aff04f73806110546866151b22653
inshalashraf <mdinshalashraf@gmail.com>
2026-09-12 16:16:43 +0530

    Add Salary account

---
+++ SalaryAccount.py
@@ -0,0 +1,7 @@
+from BankAccount import BankAccount
+
+
+class SalaryAccount(BankAccount):
+
+    def __init__(self, account_number, name, age, balance):
+        super().__init__(account_number, name, age, balance, "Salary")
