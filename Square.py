f57b71e97a38dd806bd729003ba0add17486f32e
inshalashraf <mdinshalashraf@gmail.com>
2026-09-12 16:22:34 +0530

    Add Square example for LSP

---
+++ Square.py
@@ -0,0 +1,12 @@
+from Rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    def set_width(self, width):
+        self.width = width
+        self.height = width
+
+    def set_height(self, height):
+        self.width = height
+        self.height = height
